f2af53f47ba6fc40e71e7da37ac9c7143a4b2318
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-12-03 18:03:27 +0700

    edit news

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -12,20 +12,21 @@ export default function NewsPage() {
     () => router.query.id && `/api/news/${router.query.id}`,
     fetcher
   )
-  console.log({ data })
   return (
-    <div className="max-w-3xl mx-auto py-10 px-6">
-      <h1 className="text-6xl font-semibold text-center mb-10">
-        {data?.title}
-      </h1>
-      <p className="text-lg text-center mb-4">{data?.review}</p>
-      <img src={data?.image} alt={data?.title} className="mb-8" />
-      {data?.body ? (
-        <div
-          className="prose lg:prose-lg mx-auto"
-          dangerouslySetInnerHTML={{ __html: data?.body }}
-        ></div>
-      ) : null}
+    <div className="bg-white py-20">
+      <div className="max-w-3xl mx-auto py-10 px-6 ">
+        <h1 className="text-6xl font-semibold text-center mb-10">
+          {data?.title}
+        </h1>
+        <p className="text-lg text-center mb-4">{data?.review}</p>
+        <img src={data?.image} alt={data?.title} className="mb-8" />
+        {data?.body ? (
+          <div
+            className="prose lg:prose-lg mx-auto"
+            dangerouslySetInnerHTML={{ __html: data?.body }}
+          ></div>
+        ) : null}
+      </div>
     </div>
   )
 }
